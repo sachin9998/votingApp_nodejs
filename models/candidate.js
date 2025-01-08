ae54d1e025eafb679413649e7b9f363c1314a9b4
Sachin Alam <sachinalam4216@gmail.com>
2025-01-08 17:22:34 +0530

    candidate schema added

---
+++ models/candidate.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+// const bcrypt = require('bcrypt');
+
+// Define the Person schema
+const candidateSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  party: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: Number,
+    required: true,
+  },
+  votes: [
+    {
+      user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+      },
+      votedAt: {
+        type: Date,
+        default: Date.now(),
+      },
+    },
+  ],
+  voteCount: {
+    type: Number,
+    default: 0,
+  },
+});
+
+export const Candidate = mongoose.model("Candidate", candidateSchema);
